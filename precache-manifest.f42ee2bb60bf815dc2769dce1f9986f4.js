0f010728f6f34c21370df72b213bda373842d7f8
Tom Deneau <tom.deneau@gmail.com>
2022-03-02 07:37:18 -0600

    Updates

---
+++ precache-manifest.f42ee2bb60bf815dc2769dce1f9986f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3029b17c99f4bc72871fbc44ccfb986",
+    "url": "/wordguess/index.html"
+  },
+  {
+    "revision": "132ab2e9d43e6b95bf6b",
+    "url": "/wordguess/static/css/2.b7633576.chunk.css"
+  },
+  {
+    "revision": "587f89488fb05c0ec50a",
+    "url": "/wordguess/static/css/main.12d42322.chunk.css"
+  },
+  {
+    "revision": "132ab2e9d43e6b95bf6b",
+    "url": "/wordguess/static/js/2.8db6ca67.chunk.js"
+  },
+  {
+    "revision": "f73ba50d6611f5c3f949957876282ee5",
+    "url": "/wordguess/static/js/2.8db6ca67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "587f89488fb05c0ec50a",
+    "url": "/wordguess/static/js/main.85e9da3f.chunk.js"
+  },
+  {
+    "revision": "5ba3b7eb275490880402",
+    "url": "/wordguess/static/js/runtime-main.5c2e9a65.js"
+  }
+]);
